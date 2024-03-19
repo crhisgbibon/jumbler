6a2d3f4d3059648144dac087f1dafefdae29da49
crhisgbibon <admin@crhisgbibon.com>
2024-03-19 22:49:16 +0000

    update

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -39,7 +39,6 @@ THEME.innerHTML = svg.theme.trim();
 
 const CANVAS = document.getElementById("CANVAS");
 const c = CANVAS.getContext("2d");
-c.canvas.willReadFrequently = true;
 
 ORIGINAL.onclick = function(){ window.jumbler.is_original() };
 JUMBLE.onclick = function(){ window.jumbler.jumble() };
@@ -59,13 +58,13 @@ class Fragment
     index,
     row,
     column,
-    imgData,
+    img_data,
   )
   {
-    this.frag = imgData;
+    this.index = index;
     this.row = row;
     this.column = column;
-    this.index = index;
+    this.frag = img_data;
   }
 }
 
@@ -103,12 +102,18 @@ class Jumbler
   toggle_dark()
   {
     this.dark = !this.dark;
+    this.draw();
   }
 
   update_menus()
   {
     if(this.dark)
     {
+      document.body.classList.remove('bg-mod_light');
+      document.body.classList.add('bg-mod_dark');
+      document.body.classList.remove('text-mod_dark');
+      document.body.classList.add('text-mod_light');
+
       NAV.classList.remove('bg-mod_light');
       MENU.classList.remove('bg-mod_light');
 
@@ -123,6 +128,11 @@ class Jumbler
     }
     else
     {
+      document.body.classList.remove('bg-mod_dark');
+      document.body.classList.add('bg-mod_light');
+      document.body.classList.remove('text-mod_light');
+      document.body.classList.add('text-mod_dark');
+
       NAV.classList.remove('bg-mod_dark');
       MENU.classList.remove('bg-mod_dark');
 
@@ -177,30 +187,34 @@ class Jumbler
   {
     if(this.uploaded_files.length == 0) return;
     this.current_img.src = this.uploaded_files[UPLOADED.value].src;
-    c.drawImage(this.current_img, 0, 0);
     this.stored_width = this.current_img.width;
     this.stored_height = this.current_img.height;
-    this.create();
+    this.init();
+    this.dimensions();
+    this.draw();
   }
 
   scale_up()
   {
     this.scale += 0.1;
     this.scale = parseFloat(this.scale.toFixed(2));
-    console.log(this.scale, 'scaling up');
     this.dimensions();
+    this.draw();
   }
 
   scale_down()
   {
     this.scale -= 0.1;
     this.scale = parseFloat(this.scale.toFixed(2));
-    console.log(this.scale, 'scaling down');
     this.dimensions();
+    this.draw();
   }
 
-  dimensions()
+  init()
   {
+    if(this.uploaded_files.length == 0) return;
+    if(this.current_img == null || this.current_img == '') return;
+
     CANVAS.width = (this.stored_width * this.scale);
     CANVAS.height = (this.stored_height * this.scale);
 
@@ -234,6 +248,8 @@ class Jumbler
     }
     else
     {
+      CANVAS.width = this.current_img.width;
+      CANVAS.height = this.current_img.height;
       c.drawImage(this.current_img, 0, 0, this.current_img.width, this.current_img.height);
     }
 
@@ -245,43 +261,134 @@ class Jumbler
     this.fragments.length = 0;
     this.original.length = 0;
 
-    if(CANVAS.width === 0) return;
-
     for(let i = 0; i < ROWS.value; i++)
     {
       for(let p = 0; p < COLUMNS.value; p++)
       {
-        let imgData = c.getImageData(i * this.tW, p * this.tH, this.tW, this.tH);
+        let img_data = c.getImageData(i * this.tW, p * this.tH, this.tW, this.tH);
+
         let f = new Fragment(
           this.fragments.length,
           i,
           p,
-          imgData,
+          img_data,
         );
+
         this.fragments.push(f);
+
         let f1 = new Fragment(
           this.fragments.length,
           i,
           p,
-          imgData,
+          img_data,
         );
+
         this.original.push(f1);
       }
     }
   }
 
-  create()
+  dimensions()
   {
-    if(this.uploaded_files.length == 0) return;
-    if(this.current_img == null || this.current_img == '') return;
+    CANVAS.width = (this.stored_width * this.scale);
+    CANVAS.height = (this.stored_height * this.scale);
 
-    this.dimensions();
+    let maxWidth = CANVAS.offsetWidth * 0.75;
+    let maxHeight = CANVAS.offsetHeight * 0.75;
+
+    let originalWidth = this.stored_width;
+    let originalHeight = this.stored_height;
+
+    console.log(this.current_img.width);
+    console.log(this.current_img.height);
+
+    if(originalWidth > maxWidth)
+    {
+      console.log('toowide');
+      let percent = ( 100 / CANVAS.width ) * maxWidth;
+      let newHeight = ( CANVAS.height / 100 ) * percent;
+
+      this.current_img.width = maxWidth;
+      this.current_img.height = newHeight;
+      CANVAS.width = maxWidth;
+      CANVAS.height = newHeight;
+      c.drawImage(this.current_img, 0, 0, maxWidth, newHeight);
+    }
+    else if(originalHeight > maxHeight)
+    {
+      console.log('toohigh');
+      let percent = ( 100 / CANVAS.height ) * maxHeight;
+      let newWidth = ( CANVAS.width / 100 ) * percent;
+
+      this.current_img.width = newWidth;
+      this.current_img.height = maxHeight;
+      CANVAS.width = newWidth;
+      CANVAS.height = maxHeight;
+      c.drawImage(this.current_img, 0, 0, newWidth, maxHeight);
+    }
+    else
+    {
+      console.log('justright');
+      CANVAS.width = this.current_img.width;
+      CANVAS.height = this.current_img.height;
+      c.drawImage(this.current_img, 0, 0, this.current_img.width, this.current_img.height);
+    }
+
+    console.log(this.current_img.width);
+    console.log(this.current_img.height);
+
+    this.tW = this.current_img.width / ROWS.value;
+    this.tH = this.current_img.height / COLUMNS.value;
+
+    let counter = 0;
+
+    for(let i = 0; i < ROWS.value; i++)
+    {
+      for(let p = 0; p < COLUMNS.value; p++)
+      {
+        let img_data = c.getImageData(i * this.tW, p * this.tH, this.tW, this.tH);
+        this.original[counter].frag = img_data;
+
+        for(let f = 0; f < this.fragments.length; f++)
+        {
+          if(this.fragments[f].frag === img_data)
+          {
+            this.fragments[f].frag = img_data;
+            break;
+          }
+        }
+        counter++;
+      }
+    }
+  }
+
+
+  draw()
+  {
+    console.log(this.show_original);
+    if(this.show_original === true)
+    {
+      for(let i = 0; i < this.original.length; i++)
+      {
+        c.putImageData(this.original[i].frag, this.original[i].row * this.tW, this.original[i].column * this.tH);
+      }
+    } 
+    else
+    {
+      for(let i = 0; i < this.fragments.length; i++)
+      {
+        c.putImageData(this.fragments[i].frag, this.fragments[i].row * this.tW, this.fragments[i].column * this.tH);
+      }
+    }
   }
 
   jumble()
   {
     if(this.uploaded_files.length == 0) return;
     if(this.fragments.length == 0) return;
+
+    let check = window.confirm('reset the current game?');
+    if(check === false) return;
 
     let temp = [];
     let tempImg = [];
@@ -303,31 +410,18 @@ class Jumbler
       c.putImageData(this.fragments[i].frag, this.fragments[i].row * this.tW, this.fragments[i].column * this.tH);
     }
     
+    this.show_original = false;
+    ORIGINAL.style.transform = '';
     this.complete();
   }
 
   is_original()
   {
     if(this.uploaded_files.length == 0) return;
-
     this.show_original = !this.show_original;
-
-    if(this.show_original === true)
-    {
-      for(let i = 0; i < this.original.length; i++)
-      {
-        c.putImageData(this.original[i].frag, this.original[i].row * this.tW, this.original[i].column * this.tH);
-      }
-      ORIGINAL.style.transform = 'rotate(90deg)';
-    } 
-    else
-    {
-      for(let i = 0; i < this.fragments.length; i++)
-      {
-        c.putImageData(this.fragments[i].frag, this.fragments[i].row * this.tW, this.fragments[i].column * this.tH);
-      }
-      ORIGINAL.style.transform = '';
-    }
+    if(this.show_original === true) ORIGINAL.style.transform = 'rotate(90deg)';
+    else ORIGINAL.style.transform = '';
+    this.draw();
   }
 
   complete()
@@ -338,11 +432,14 @@ class Jumbler
       if(this.fragments[i].frag === this.original[i].frag) score++;
     }
     let percent = Math.floor(100 / this.fragments.length * score);
+    if(isNaN(percent)) percent = 0;
     COMPLETED.innerHTML = percent.toString() + "%";
+    if(percent === 100) window.alert('well done xd');
   }
 
   mouse_down(x, y)
   {
+    if(this.show_original === true) return;
     let segX = Math.floor(x / this.tW);
     let segY = Math.floor(y / this.tH);
     this.firstP = segX * COLUMNS.value + segY;
@@ -350,6 +447,7 @@ class Jumbler
 
   mouse_up(x, y)
   {
+    if(this.show_original === true) return;
     let segX = Math.floor(x / this.tW);
     let segY = Math.floor(y / this.tH);
     this.secondP = segX * COLUMNS.value + segY;
@@ -369,19 +467,11 @@ class Jumbler
 
 CANVAS.onmousedown = function(event) {
   event.preventDefault();
-
-  // Get the scroll position
   const scrollX = window.pageXOffset || document.documentElement.scrollLeft;
   const scrollY = window.pageYOffset || document.documentElement.scrollTop;
-
-  // Get the bounding rectangle of the canvas
   let rect = CANVAS.getBoundingClientRect();
-
-  // Adjust mouse coordinates for scroll position
   let x = event.clientX - rect.left - scrollX;
   let y = event.clientY - rect.top - scrollY;
-
-  // Pass the adjusted coordinates to your function
   window.jumbler.mouse_down(x, y);
 };
 
@@ -389,14 +479,9 @@ CANVAS.onmousedown = function(event) {
 CANVAS.onmouseup = function(event)
 {
   event.preventDefault();
-  // Get the scroll position
   const scrollX = window.pageXOffset || document.documentElement.scrollLeft;
   const scrollY = window.pageYOffset || document.documentElement.scrollTop;
-
-  // Get the bounding rectangle of the canvas
   let rect = CANVAS.getBoundingClientRect();
-
-  // Adjust mouse coordinates for scroll position
   let x = event.clientX - rect.left - scrollX;
   let y = event.clientY - rect.top - scrollY;
   window.jumbler.mouse_up(x, y);
